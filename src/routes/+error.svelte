fbfee3a6f77790b503b71eed5811d1eb98e846a7
João Vitor M.L <doutnus@tutanota.com>
2024-02-05 22:52:51 -0300

    Hemos añadido la página de error

---
+++ src/routes/+error.svelte
@@ -0,0 +1,12 @@
+<div> 
+	<h2 class="text-5xl font-medium font-workbench">OOOOPS!</h2>
+	<span class="mt-4">¡No hemos podido encontrar la página que busca! ¡Lo sentimos! :c</span>
+	<div>
+		<a href="/">Volver</a>
+	</div>
+</div>
+
+<style>
+	div { width: 1410px; margin: 6rem auto;}
+	a { text-decoration: underline; }
+</style>
